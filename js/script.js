ad977b79b30d89674bd28c6683fc7a4b49502530
Young Sam Concepts <youngsamconcepts@gmail.com>
2024-04-01 14:09:55 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function userScroll() {
+  const navbar = document.querySelector('.navbar');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 10) {
+      navbar.classList.add('bg-dark');
+    } else {
+      navbar.classList.remove('bg-dark');
+    }
+  });
+  window.addEventListener('DOMContentLoaded', () => {
+    if (window.scrollY > 10) {
+      navbar.classList.add('bg-dark');
+    } else {
+      navbar.classList.remove('bg-dark');
+    }
+  });
+}
+
+document.addEventListener('DOMContentLoaded', userScroll);
